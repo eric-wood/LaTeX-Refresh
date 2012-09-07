bd9dcf371fc6f138bba2c326dbaf0e41cdef1e5c
Eric Wood <eric@woodtx.com>
2012-09-07 11:00:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ LaTeX-Refresh
 =============
 
 Monitor modifications to a LaTeX file and re-render - real-time view of your changes!
+
+### Usage
+Just put ```refresh_latex.rb``` somewhere in your path and pass it the LaTeX file you'd like to use. That's it!
+
+### Notes
+Right now it's wired up to support (Skim.app)[http://skim-app.sourceforge.net/] - in order for you to get it to work you'll need to modify the ```pdf_viewer``` variable defined at the top of the file.
+
+It's easy to make it work with other PDF viewers, and if you want to use Preview.app take note of the code I commented out...there's a special little hack that you need to use which I've thrown in. Most viewers have the ability to automatically reload the PDF, though.
+
+This script only supports OS X right now, but that's okay, since it'd be very very simple to port it to any other platform! The ```open``` command is OS X-specific, and to my knowledge its Linux counterparts are desktop environment dependent.
+
+
+### Dependencies
+Since OS X doesn't have inotify, I used the handy (listen gem)[http://rubygems.org/gems/listen]
+
+### License
+Do as you please, but don't be a dick about it! I'd love to hear everyone's thoughts and will gladly accept pull requests :)
+
+Happy hacking!
---
+++ refresh_latex.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/env ruby
+
+#==========================================================================
+# refresh_latex
+# hastily thrown together by Eric Wood (http://ericwood.org)
+#==========================================================================
+# Automagically re-render LaTeX files and open the result in a PDF reader.
+# Using Skim we can do all of this without losing focus to the editor,
+# although I provide code to somewhat accomplish this with Preview.app
+#
+# As of right now it only works in OS X since it uses the "open" command.
+# Feel free to port it to any platform you want to
+#
+# NOTE: requires the "listen" ruby gem
+# Install it with the following command:
+# gem install listen
+#==========================================================================
+
+require 'listen'
+
+if ARGV.size == 0
+  puts "You didn't tell me the LaTeX document you wanted to render!"
+  exit 0
+end
+
+# Path to the PDF viewer you want to use!
+pdf_viewer = '/Users/eric/Applications/Skim.app'
+
+# Input argument is the path to the tex file
+filename = File.absolute_path(ARGV[0])
+
+# Name without extension
+name = File.basename(filename).chomp(File.extname(filename))
+
+output_dir = File.dirname(filename)
+output_file = File.join(output_dir, "#{name}.pdf")
+
+# These temp thingies are for if there was that option to throw
+# the auxillary files there or whatever. Meh.
+temp_dir = '/tmp/latex_refresh'
+
+# Create temp directory if it doesn't already exist
+Dir.mkdir(temp_dir) unless File.exists?(temp_dir)
+
+# Set output directory and place aux files in /tmp
+options = [
+  #"-aux-directory=#{temp_dir}", # This doesn't work for some reason :(
+  "-output-directory=#{output_dir}"
+]
+
+# The rest of the arguments are options that can be passed into pdflatex
+options += ARGV[1..-1] if ARGV.size > 1
+
+def render(filename, options)
+  #`pdflatex '#{filename}' #{options.join(' ')} 2>&1 /dev/null`
+  `pdflatex '#{filename}' #{options.join(' ')}`
+end
+
+def open(output_file, pdf_viewer)
+  # If you want to open it with Skim.app us this line
+  # Redirect output to /dev/null since Skim.app complains a lot even if it's working
+  `open -g -a '#{pdf_viewer}' '#{output_file}' 2>&1 /dev/null`
+
+  # Preview.app only refreshes the PDF when we give it focus,
+  # so we'll use this hack to give it focus and return focus back
+  # to the application that was open before.
+  # If you want to use a different viewer you can get rid of all this...
+  #
+  ## Get the currently focused application
+  #app = `osascript -e 'POSIX path of (path to application (name of (info for (path to frontmost application))))'`.strip
+  #
+  ## Open the PDF
+  #`open '#{output_file}'`
+  #
+  ## Restore focus to the original app
+  #`osascript -e 'tell application "#{app}" to activate'`
+end
+
+# Render and open the pdf for the first time
+render(filename, options)
+open(output_file, pdf_viewer)
+
+# Listen for changes to the tex file
+Listen.to(File.dirname(filename), :filter => /\.tex$/) do |modified, added, removed|
+  # According to Matt this has to happen at least 2 times. Whatever.
+  render(filename, options)
+end
+
